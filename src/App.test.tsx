46130b5bc6f62f7b21d3ff72d95a5beef050f019
Joseph Stahl <josephst@users.noreply.github.com>
2017-07-27 17:37:37 -0500

    use enzyme for shallow testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as enzyme from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const div = enzyme.shallow(<App />);
+  expect(div.find('h2').text()).toEqual('Welcome to React');
 });
