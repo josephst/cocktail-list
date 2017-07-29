f6e97569595116079f2446b87b7913cabc3cd596
Joseph Stahl <hello@josephstahl.com>
2017-07-29 00:43:17 -0500

    fix type error with yarn

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ class App extends React.Component<{}, {}> {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Cocktails</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          Intro.
         </p>
         <DrinkList />
       </div>
