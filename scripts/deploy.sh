a17c1a8a3bab2865523e995bab392c9907e18c20
Joseph Stahl <hello@josephstahl.com>
2017-09-04 11:34:48 -0500

    fix cp command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-aws s3 cp build/ s3://cocktails.josephstahl.com
+aws s3 cp build/ s3://cocktails.josephstahl.com --recursive
 aws s3 sync build/ s3://cocktails.josephstahl.com --delete
 aws s3 cp s3://cocktails.josephstahl.com/index.html s3://cocktails.josephstahl.com/index.html --metadata-directive REPLACE --cache-control max-age=3600
 aws s3 cp s3://cocktails.josephstahl.com/data/db.json s3://cocktails.josephstahl.com/data/db.json --metadata-directive REPLACE --cache-control max-age=3600
