546f83aa7ba39f7209f3c9ccef87ee4916e7e424
Joseph Stahl <josephst@users.noreply.github.com>
2017-09-08 17:33:48 -0500

    make all files publically readable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-aws s3 sync build/ s3://cocktails.josephstahl.com --delete --cache-control max-age=43200,public
+aws s3 sync build/ s3://cocktails.josephstahl.com --delete --cache-control max-age=43200,public --acl public-read
 aws s3 cp s3://cocktails.josephstahl.com/service-worker.js s3://cocktails.josephstahl.com/service-worker.js --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type application/javascript --acl public-read
 aws s3 cp s3://cocktails.josephstahl.com/index.html s3://cocktails.josephstahl.com/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
 aws s3 cp s3://cocktails.josephstahl.com/data/db.json s3://cocktails.josephstahl.com/data/db.json --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type application/json --acl public-read
